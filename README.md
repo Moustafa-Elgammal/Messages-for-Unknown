2128a410cb0d501ca21b03b5c85628f187bda589
Moustafa Mohammed <moustafa_algammal@yahoo.com>
2018-02-08 17:49:35 +0200

    init mobile app

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ web app for sending WhatsApp messages for unsaved contacts
 - [x] open in new table if deskt
 
 ### open mobile App
-- [ ]  switch to the mobile app.
+- [x]  switch to the mobile app.
--- js/main.js
+++ js/main.js
@@ -5,44 +5,88 @@
 */
 
 jQuery(document).ready(function($){
-	// https://api.whatsapp.com/send?phone=whatsappphonenumber&text=urlencodedtext
-	var openInWeb = function(phone, message){
-		var url = "https://web.whatsapp.com/send";
+	
+	/**
+	*	Request function
+	*	@phone single phone number
+	*	@message text messages
+	*	@url correct url
+	*/
+	var openIn = function(phone, message, url){
+
+		// assimble the full url
 		url = url + "?phone=" + phone + "&text=" + message;
+		// open
 		window.open(url, '_blank');
 	}
 
+	/**
+	*	return the correct uri 
+	*	Browser 
+	*	Mobile App
+	*/
+	var urlCreation = function (){
+
+		//	get the custom uri
+		if(navigator.appVersion.indexOf("Win") == -1){
+			// for Mobile App
+			return "whatsapp://send";
+		}
+		else
+		{	
+			// fro web browser
+			return "https://web.whatsapp.com/send";
+		}
+	}
+
+	/**
+	*	clickSubmit
+	*	form submitted
+	*/
 	var clickSubmit = function(e){
 
-		e.preventDefault();
-		var phonesArray = $('#phones').val().split(',');
+		// Disable custom events of the form
+		e.preventDefault();	
+
+		//get array of phone numbers
+		var phonesArray = $('#phones').val().split(','); 
+
+		//get message text and encode
 		var messageText = encodeURIComponent($('#message').val());
+
+		//set delay to exeute each message
 		var timeDelay = parseInt($('#delay').val());
 
 		try{
-
+			// check at least phone number
 			if(!phonesArray[0].length){
 				throw "at least input one phone number.";
 			}
 
+			//	handle empty messages
 			if(!messageText.length){
 				throw "Empty Message";
 			}
 
+			var x = 0; // no delay for first number
 			phonesArray.forEach(function(phone){
+
+				//	fire the messages
 				setTimeout(function() {
-  					openInWeb(phone, messageText);
-				}, timeDelay * 1000);
+					x= timeDelay * 1000; //	change delay
+					url = urlCreation(); // get the custom uri
+  					openIn(phone, messageText, url); // open 
+				}, x);
 
 			});
 
 		}
-		catch (err){
-			console.log("Excption: " + err);
+		catch (err){ // handle rules exception
+			alert("Exception: " + err);
 		}
 
 	}
 
-
+	// Execute the app
 	$('#submit').on('click', function (e){clickSubmit(e);});
 });
